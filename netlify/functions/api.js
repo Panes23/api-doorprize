ab870c8a00d806dbcbbf636efb248d15867e2760
poorgas <boss.rangga23@gmail.com>
2025-03-22 16:47:30 +0700

    update status

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -148,7 +148,7 @@ router.post("/vouchers", authenticateApiRequest, async (req, res) => {
         // Validasi nominal
         if (nominal < configData.minimal_nominal) {
             return res.status(402).json({ 
-                error: `Lakukan pemasangan setelah diskon lebih dari Rp.${configData.minimal_nominal} Untuk mendapatkan voucher Doorprize.` 
+                error: `Lakukan 1 invoice pemasangan setelah diskon lebih dari Rp.${configData.minimal_nominal} Untuk mendapatkan voucher Doorprize.` 
             });
         }
 
